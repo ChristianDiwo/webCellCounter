79ed433014432608b30ed5833cf724f74b3a3841
ChristianDiwo <chr.diwo@gmail.com>
2023-03-16 10:02:27 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
+const constraints = { 
+  video: {
+    facingMode: "user",
+    frameRate: { ideal: 15, max: 15 },
+    width: { ideal: 640, max: 640 },
+    height: { ideal: 480, max: 480 }
+  }, 
+  audio: false 
 };
-// Start the video stream when the window loads
+// Define constants
+const constraints = { video: { facingMode: "user" }, audio: false };
+const cameraView = document.querySelector("#camera--view");
+const cameraOutput = document.querySelector("#camera--output");
+const cameraSensor = document.querySelector("#camera--sensor");
+const cameraTrigger = document.querySelector("#camera--trigger");
+
+async function cameraStart() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    track = stream.getTracks()[0];
+    cameraView.srcObject = stream;
+  } catch (error) {
+    console.error("Oops. Something is broken.", error);
+  }
+}
+
 window.addEventListener("load", cameraStart, false);
