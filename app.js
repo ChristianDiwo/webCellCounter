cba6c045678371acdd3480a1f5942fcf7ebcaf0f
ChristianDiwo <chr.diwo@gmail.com>
2023-03-16 10:43:20 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Set constraints for the video stream
 const constraints = { 
   video: {
-    deviceId: { exact: "", ideal: "" },
+    facingMode: 'environment',
     frameRate: { ideal: 15, max: 15 },
     width: { ideal: 640, max: 640 },
     height: { ideal: 480, max: 480 }
@@ -13,19 +13,6 @@ const cameraView = document.querySelector("#camera--view");
 const cameraOutput = document.querySelector("#camera--output");
 const cameraSensor = document.querySelector("#camera--sensor");
 const cameraTrigger = document.querySelector("#camera--trigger");
-
-async function getBackCamera() {
-  const devices = await navigator.mediaDevices.enumerateDevices();
-  const backCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
-
-  if (backCamera) {
-    constraints.video.deviceId.exact = backCamera.deviceId;
-    return true;
-  } else {
-    console.error("Could not find back camera");
-    return false;
-  }
-}
 
 async function cameraStart() {
   try {
