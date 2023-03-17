80ca8721cba0a4e7ad5301ce5eaf7073a125ad2e
ChristianDiwo <chr.diwo@gmail.com>
2023-03-17 17:26:02 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Set constraints for the video stream
 const constraints = { 
   video: {
-    facingMode: 'environment',
+    deviceId: { 
+      exact: "",
+      ideal: ""
+    },
     frameRate: { ideal: 15, max: 15 },
     width: { ideal: 640, max: 640 },
     height: { ideal: 480, max: 480 }
@@ -14,13 +17,29 @@ const cameraOutput = document.querySelector("#camera--output");
 const cameraSensor = document.querySelector("#camera--sensor");
 const cameraTrigger = document.querySelector("#camera--trigger");
 
+async function getBackCamera() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const backCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
+
+  if (backCamera) {
+    constraints.video.deviceId.exact = backCamera.deviceId;
+    return true;
+  } else {
+    console.error("Could not find back camera");
+    return false;
+  }
+}
+
 async function cameraStart() {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    track = stream.getTracks()[0];
-    cameraView.srcObject = stream;
-  } catch (error) {
-    console.error("Oops. Something is broken.", error);
+  const hasBackCamera = await getBackCamera();
+  if (hasBackCamera) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      track = stream.getTracks()[0];
+      cameraView.srcObject = stream;
+    } catch (error) {
+      console.error("Oops. Something is broken.", error);
+    }
   }
 }
 
